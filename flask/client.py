7dcf7c8b292dc9a36bdf893ccd4ed90e30018a59
bilalahmedkhatri <bilalahmedkhatri@outlook.com>
2024-03-13 07:24:45 +0500

    lzma perfectly working, get size of video, trying to build video from iamges

--- flask/client.py
+++ flask/client.py
@@ -1,13 +1,28 @@
 import asyncio
 import websockets
 import mss
-import numpy as np
-import cv2
 import socket
 import json
-import uuid
-import zlib
 import lzma
+from os import environ
+from sys import platform
+from time import time
+
+
+class ClientMonitorTools:
+
+    def get_start_time(self):
+        return time()
+
+    def get_system_ip(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        ip = sock.connect(("8.8.8.8", 80))
+        sock.close()
+        return ip
+
+    def get_username(self) -> None:
+        self.USER_NAME = environ["USERNAME"] if platform.startswith(
+            'win') else environ["USER"]
 
 
 def detect_screens(screens) -> list:
@@ -18,25 +33,11 @@ def detect_screens(screens) -> list:
     return screen
 
 
-def get_system_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    ip = s.getsockname()
-    s.close()
-    return ip
-
-
 async def send_images(websocket):
 
     with mss.mss() as sct:
         # Detect screens
         detect_screen = detect_screens(sct)
-
-        ui_ = uuid.uuid4()
-
-        # Create a image compress object
-        image_encode_compress = [int(cv2.IMWRITE_JPEG_QUALITY), 70]
-
         try:
             while True:
                 for scn in detect_screen:
@@ -46,49 +47,32 @@ async def send_images(websocket):
                     if not screenshot:
                         break
 
-                     # Convert the screenshot to a numpy array, then to bytes
-                    # img_array = np.array(screenshot)
-                    # frame = cv2.cvtColor(img_array, cv2.COLOR_BGRA2BGR)
-                    # _, buffer = cv2.imencode(
-                    #     '.jpg', frame, image_encode_compress)
-                    # array_to_bytes = buffer.tobytes()
+                    print(screenshot.size)
 
                     # Send the image data to the server
-                    compressed_lmza = lzma.compress(screenshot.raw)
+                    compressed_lmza = lzma.compress(screenshot.rgb)
                     data = json.dumps({
                         'image': compressed_lmza.decode("latin-1"),
                         'status': True,
-                        'uid': str(get_system_ip())
+                        'size': screenshot.size
                     })
                     x = len(compressed_lmza.decode("latin-1"))
-                    print('lenl', x)
                     await websocket.send(data)
                     recv_data = await websocket.recv()
                     print(f"receve :", recv_data)
-                    # await asyncio.sleep(0.08)
-                    await asyncio.sleep(2)
+                    await asyncio.sleep(0.7)
 
         except websockets.exceptions.ConnectionClosed as e:
             print("WebSocket connection closed.", e)
 
-        finally:
-            cv2.destroyAllWindows()
-
 
 async def main():
     url = "ws://localhost:8005"
-    # url = f"ws://localhost:8005/"
-    # uri = f"ws://localhost:8000/ws/video/"
     # uri = f"wss://192.168.1.85:8088" # workin fine with IP address
-    user_id = str(uuid.uuid4())  # Generate unique user ID
-    # async with websockets.connect(url, headers={'user_id': user_id}, ping_interval=None, ping_timeout=50) as websocket:
     async with websockets.connect(url, ping_interval=None, ping_timeout=50) as websocket:
         print(f"Connected to {url}, Local system IP address ")
-
-        # Send images continuously to the server
         await send_images(websocket)
 
 
 if __name__ == "__main__":
-    # Run the WebSocket client
     asyncio.get_event_loop().run_until_complete(main())
--- flask/server_2.py
+++ flask/server_2.py
@@ -1,78 +1,52 @@
 import asyncio
 import websockets
-import numpy
 import cv2
-import socket
 import numpy as np
-import base64
 import json
 import lzma
 
 
-async def build_video():
-    pass
+class ServerMonitorTools:
 
+    def __init__(self, *args, **kwargs):
+        self.frames = []
+        self.websocket = kwargs["websocket"]
+        self.fourcc = cv2.VideoWriter_fourcc(*'XVID')  # Video codec
 
-def get_system_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    ip = s.getsockname()
-    s.close()
-    return ip
+    async def get_socket_from_websocket(self) -> list:
+        data = await self.websocket.recv()
+        data_dict = json.loads(data)
+        compressed_raw_image = data_dict['image'].encode(
+            "latin-1")  # decoded string in latin-1
+        decompress_image = lzma.decompress(
+            compressed_raw_image)  # comporessed image frame
+        return [decompress_image, data_dict['status'], data_dict['size']]
+
+    async def build_video(self):
+        image_bytes = await self.get_socket_from_websocket()
+        out = cv2.VideoWriter('video.mp4', self.fourcc,
+                              20.0, (image_bytes[2][0], image_bytes[2][1]))
+        to_np_array = np.array(image_bytes[0])
+
+        # image_array = np.frombuffer(image_bytes[1], dtype=np.uint8)
+        # print('firet ', image_array)
+        # to_image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+        # print(to_image)
+        # frame = cv2.cvtColor(to_image, cv2.COLOR_BGRA2BGR)
+        # cv2.imshow('video', image_array)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
 
 
 async def server(websocket, path):
-    # print(f"Client connected: {websocket.remote_address}")
-
-    # Set the codec and create a VideoWriter object
-    # fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    # out = cv2.VideoWriter('output_video.avi', fourcc, 30.0, (640, 480))
-    # ip_, port_ = websocket.remote_address
-    # print(f"Client connected: {ip_}:{port_}")
-    # Open the stream
-    # print('', websocket.request_headers)
+    monitor_tools = ServerMonitorTools(websocket=websocket)
     try:
         while True:
-            # Continuously listen for messages from the client
-            # async for message in websocket:
-
-            # Convert the screenshot to a numpy array, then to bytes
-            # img_array = np.array(msg)
-            # frame = cv2.cvtColor(img_array, cv2.COLOR_BGRA2BGR)
-            # _, buffer = cv2.imencode(
-            #     '.jpg', frame, image_encode_compress)
-            # array_to_bytes = buffer.tobytes()
-
-            # bytes_to_array = numpy.frombuffer(message, numpy.uint8)
-            # to_image = cv2.imdecode(frame, cv2.IMREAD_COLOR)
-
-            # testing images in video frames
-            # cv2.imshow("screenshot_video", to_image)
-            # cv2.waitKey(1)
-            # d_bytes = message.decode('utf-8')
-            # ds = base64.dncodebytes(message)
-            # str_to_bytearray = message.encode('utf-8')
-            # print(f"received: {await message.recv()}")
-            data = await websocket.recv()
-            data_dict = json.loads(data)
-            status = data_dict['status']
-            compressed_raw_image = data_dict['image'].encode("latin-1")
-            print('type comp: ', type(compressed_raw_image),
-                  'len :', len(compressed_raw_image))
-
-            decompress_image = lzma.decompress(compressed_raw_image)
-            print('type decomp: ', type(decompress_image),
-                  'len :', len(decompress_image))
-            str_to_decompress = data.encode("latin-1")
-
+            await monitor_tools.build_video()
             await websocket.send("received...")
-            # out.write(bytes_to_array)
-
     except websockets.exceptions.ConnectionClosed:
         print(f"Client disconnected: {websocket.remote_address}")
 
-    # finally:
-        # cv2.destroyAllWindows()
 
 # Start the WebSocket server on localhost, port 8080
 if __name__ == "__main__":
